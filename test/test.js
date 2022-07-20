d6d9f74c885ba02aa69ba57dca98bbb10dfd9bc2
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-07-20 16:48:30 +0200

    Created the test.js

---
+++ test/test.js
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+import {
+  addActivity, elimanateOne, removeone, eliminateCompleteds,
+} from '../src/addEditErase.js';
+
+
+document.body.innerHTML = '<ul class="todos"></ul>';
+
+// Tests for Adding item in todo list
+describe('By adding item in todos', () => {
+  // Arrange
+  const items = ['new item 1', 'new item 2', 'new item 3'];
+  // Act
+  items.forEach((item) => addActivity(item));
+  // Assert
+  test('check if addActivity is a function', () => { 
+    expect(typeof addActivity).toBe('function'); 
+   });
+  test('test if new item added', () => { 
+    expect(todos.length).not.toBe(0); 
+  });
+});
+
+// Tests for Removing item in todo list
+describe('By removing item in todos', () => {
+  // Act
+    removeone(todos.length);
+  // Assert
+  test('check if removeItem is a function', () => {
+    expect(typeof removeone).toBe('function'); 
+  });
+  test('test if item removed from array', () => { 
+    expect(todos.length).toBe(2); 
+   });
+});
